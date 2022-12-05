10fa2c68d6ce5e4f7ee2408d89edcaffee15c2d7
Zain-Ashfaq <zashfaq00@gmail.com>
2022-12-05 15:34:51 +0000

    another commit

--- src/Container/Home.jsx
+++ src/Container/Home.jsx
@@ -12,6 +12,11 @@ function Home() {
         <p className="movie">movie 3</p>
         <p className="movie">movie 4</p>
         <p className="movie">movie 5</p>
+        <p className="movie">movie 6</p>
+        {/* <p className="movie">movie 7</p>
+        <p className="movie">movie 8</p>
+        <p className="movie">movie 9</p>
+        <p className="movie">movie 10</p> */}
       </section>
     </>
   );
