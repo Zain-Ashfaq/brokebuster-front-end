df98bfe568462bb648d3eb344fe2fd68d2c3623f
Zain-Ashfaq <zashfaq00@gmail.com>
2022-12-05 15:07:07 +0000

    Homepage layout for movie with flexbox

---
+++ src/Container/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <>
+      <nav className="navbar">
+        <h1>Top movie db</h1>
+      </nav>
+      <section className="movie-list">
+        <p className="movie">movie 1</p>
+        <p className="movie">movie 2</p>
+        <p className="movie">movie 3</p>
+        <p className="movie">movie 4</p>
+        <p className="movie">movie 5</p>
+      </section>
+    </>
+  );
+}
+
+export default Home;
